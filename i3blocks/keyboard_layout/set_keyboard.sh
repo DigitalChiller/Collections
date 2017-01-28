4d26db011895e435a33321871dbd52062a449976
DigitalChiller <DigitalChiller@users.noreply.github.com>
2017-01-28 14:19:18 +0100

    added glpv3 licence

--- i3blocks/keyboard_layout/set_keyboard.sh
+++ i3blocks/keyboard_layout/set_keyboard.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+## LICENSE
+#    This file is part of Collections.
+#
+#    Collections is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Collections is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Collections.  If not, see <http://www.gnu.org/licenses/>.
 FILE="$HOME/.config/i3/current_layout"
 FIRST_LAYOUT="us"
 SECOND_LAYOUT="de"
